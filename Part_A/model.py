b7c0e24ccfc3ec634b6a7ab5d03c66f30077d07b
Ramkumar R <104501631+ramkumarravi49@users.noreply.github.com>
2025-04-19 22:47:17 +0530

    Add files via upload

--- Part_A/model.py
+++ Part_A/model.py
@@ -1,41 +1,50 @@
 import torch.nn as nn
 
 class CNN(nn.Module):
-    def __init__(self, num_classes=10, filter_size=3, activation_fn=nn.ReLU, dense_neurons=128,
-                 dropout=0.2, batchnorm=False, filter_organization='16_256'):
+    def __init__(self, filter_size=3, filter_organization='16_256', num_classes=10,   
+                 dense_neurons=256, activation_fn=nn.Mish, dropout=0.0, batchnorm=True):
         super(CNN, self).__init__()
         layers = []
         in_channels = 3
 
-        # Predefined filter organization
-        filter_counts_map = {
-            "16_256": [16, 32, 64, 128, 256],
-            "32_512": [32, 64, 128, 256, 512],
-            "constant_32": [32, 32, 32, 32, 32],
-            "pyramid_256_16": [256, 128, 64, 32, 16],
-            "64_1024": [64, 128, 256, 512, 1024],
-            "pyramid_128_8": [128, 64, 32, 16, 8],
-            "pyramid_512_32": [512, 256, 128, 64, 32],
-            "hybrid_32_64": [32, 64, 128, 64, 32],
-            "hybrid_16_256_64": [16, 32, 64, 128, 64],
-            # Adding support for the old filter organization options
-          
-        }
+        # Use predefined filter configurations
+        if filter_organization == "16_256":
+            filter_counts = [16, 32, 64, 128, 256]
+        elif filter_organization == "32_512":
+            filter_counts = [32, 64, 128, 256, 512]
+        elif filter_organization == "constant_32":
+            filter_counts = [32] * 5
+        elif filter_organization == "pyramid_256_16":
+            filter_counts = [256, 128, 64, 32, 16]
+        elif filter_organization == "64_1024":
+            filter_counts = [64, 128, 256, 512, 1024]
+        elif filter_organization == "pyramid_128_8":
+            filter_counts = [128, 64, 32, 16, 8]
+        elif filter_organization == "pyramid_512_32":
+            filter_counts = [512, 256, 128, 64, 32]
+        elif filter_organization == "hybrid_32_64":
+            filter_counts = [32, 64, 128, 64, 32]
+        elif filter_organization == "hybrid_16_256_64":
+            filter_counts = [16, 32, 64, 128, 64]
+        else:
+            filter_counts = [16, 32, 64, 128, 256]  # default
 
-        # Get filter counts based on the selected filter organization
-        filter_counts = filter_counts_map.get(filter_organization, [16, 32, 64, 128, 256])
-
-        for filters in filter_counts:
-            layers.append(nn.Conv2d(in_channels, filters, kernel_size=filter_size, padding=filter_size//2))
+        # Convolutional layers
+        for out_channels in filter_counts:
+            layers.append(nn.Conv2d(in_channels, out_channels, kernel_size=filter_size, padding=filter_size // 2))
             if batchnorm:
-                layers.append(nn.BatchNorm2d(filters))
+                layers.append(nn.BatchNorm2d(out_channels))
             layers.append(activation_fn())
             layers.append(nn.MaxPool2d(2))
-            in_channels = filters
+            in_channels = out_channels
 
         self.conv = nn.Sequential(*layers)
-        self.flattened_size = filter_counts[-1] * (128 // (2**5))**2
 
+        # Flatten size: image shrinks by 2^5 due to 5 maxpool layers
+        final_feat_dim = 128 // (2 ** 5)
+        self.flattened_size = filter_counts[-1] * final_feat_dim * final_feat_dim
+
+        # Fully connected layers
         self.fc = nn.Sequential(
             nn.Flatten(),
             nn.Linear(self.flattened_size, dense_neurons),
